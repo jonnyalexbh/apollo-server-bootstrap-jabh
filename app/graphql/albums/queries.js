8207ead23e079779ebe5f1521d5b08abc709e2cc
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-08-28 17:45:00 -0500

    return an album from the query album

--- app/graphql/albums/queries.js
+++ app/graphql/albums/queries.js
@@ -1,12 +1,14 @@
 const { gql } = require('apollo-server'),
-  { getAlbums } = require('../../services/album');
+  { getAlbum, getAlbums } = require('../../services/album');
 
 module.exports = {
   queries: {
+    album: async (_, params) => await getAlbum(params),
     albums: async () => await getAlbums(),
   },
   schema: gql`
     extend type Query {
+      album(id: ID): Album!
       albums: [Album]
     }
   `
--- app/services/album.js
+++ app/services/album.js
@@ -1,5 +1,13 @@
 const axios = require('axios');
 
+exports.getAlbum = params =>
+  axios
+    .get(`https://jsonplaceholder.typicode.com/albums/${params.id}`)
+    .then(response => response.data)
+    .catch(() => {
+      throw new Error('Cannot fetch album from external api');
+    });
+
 exports.getAlbums = () =>
   axios
     .get(`https://jsonplaceholder.typicode.com/albums`)
