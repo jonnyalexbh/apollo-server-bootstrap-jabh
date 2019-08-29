76f5b17ca7adb5758de7b5c6b5ab7d96177aa9f2
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-08-29 13:18:58 -0500

    implement logger wolox

--- app/services/album.js
+++ app/services/album.js
@@ -1,18 +1,21 @@
 const axios = require('axios');
 const config = require('../../config');
+const logger = require('../logger');
 
 const baseUri = config.common.albumsApi.url;
 
 exports.getAlbum = (params) => axios
   .get(`${baseUri}/albums/${params.id}`)
   .then((response) => response.data)
-  .catch(() => {
+  .catch((error) => {
+    logger.error(error);
     throw new Error('Cannot fetch album from external api');
   });
 
 exports.getAlbums = () => axios
   .get(`${baseUri}/albums`)
   .then((response) => response.data)
-  .catch(() => {
+  .catch((error) => {
+    logger.error(error);
     throw new Error('Cannot fetch albums from external api');
   });
