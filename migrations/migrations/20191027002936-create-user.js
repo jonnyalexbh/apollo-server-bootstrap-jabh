4ce73d446d16560bfe391707dc03d0fcf605d099
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-10-26 19:57:53 -0500

    create user migration and configure postgres

---
+++ migrations/migrations/20191027002936-create-user.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    first_name: {
+      type: Sequelize.STRING,
+    },
+    last_name: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    created_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updated_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface) => queryInterface.dropTable('users'),
+};
