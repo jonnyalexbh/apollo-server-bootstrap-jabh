51b84fed6e8dd731d98fe14e85e22084328a9219
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-10-26 20:16:32 -0500

    configure database connection data in .env

--- config/index.js
+++ config/index.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const config = {
   common: {
+    database: {
+      name: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
     albumsApi: {
       url: process.env.PLACEHOLDER_URL,
     },
